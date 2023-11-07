dc60c823479004b41b4c05e8a8bdc60b4cc2d817
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-11-06 20:50:50 -0800

    Fix CORS security error

--- app.js
+++ app.js
@@ -8,4 +8,12 @@ app.use(bodyParser.json()); // application/json
 
 app.use("/api",routes);
 
-app.listen(8080);
+// دادن مجوز های لازم برای ارسال درخواست از کلاینت های مختلف
+app.use((req , res , next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATH");
+    res.setHeader("Access-Control-Allow-Headers","Content-Type,Authorization");
+    next();
+});
+
+app.listen(8080,() => {console.log("connected...")});
