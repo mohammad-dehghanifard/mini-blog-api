2f775b15e9bd3a7eb12dfd9e3704cda98c3e2909
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-11-07 00:16:04 -0800

    Data validation when creating a new post

--- controllers/blog_controller.js
+++ controllers/blog_controller.js
@@ -1,3 +1,5 @@
+const {validationResult} = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,9 +18,19 @@ exports.getPosts = (req, res, next) => {
 }
 
 exports.createPost = (req, res, next) => {
+    //validation
+  const errors = validationResult(req); 
+
+  if(!errors.isEmpty()){
+        return res.status(422).json({
+            message : "داده های ارسال شده نا معتبر میباشند!",
+            Invalidatproperty : errors.array()[0].path,
+            error : errors.array()[0]
+        });
+  }
+
   const title = req.body.title
   const content = req.body.content
-
   res.status(201).json({
     message: 'post created successfully',
     post: {
--- routers/blog_api.js
+++ routers/blog_api.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const blogController = require("../controllers/blog_controller");
+const {body} = require("express-validator");
 
 const router = express.Router();
 
 // دریافت تمام مقالات
 router.get("/posts",blogController.getPosts);
 //ایجاد مقاله جدید
-router.post("/add-post",blogController.createPost);
+router.post("/add-post",[
+    body("title").trim().isLength({min:5}),
+    body("content").trim().isLength({min:5}),
+],blogController.createPost);
 
 module.exports = router;
